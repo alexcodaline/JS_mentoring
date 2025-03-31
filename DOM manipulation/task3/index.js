5c92053d2d95340fd4979d06b517681badd691ec
alexcodaline <aleksander.ponom@gmail.com>
2025-03-31 15:50:31 +0300

    task3

--- DOM manipulation/task3/index.js
+++ DOM manipulation/task3/index.js
@@ -2,6 +2,7 @@ const form = document.getElementById("form");
 const phoneNumb = document.getElementById("phoneNumb");
 const nameContact = document.getElementById("nameContact");
 const phoneBook = document.getElementById("phonebook");
+const search = document.getElementById("search");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -9,7 +10,7 @@ form.addEventListener("submit", (e) => {
     alert("Form is empty");
     return;
   }
-//------------------------------------------add new contact
+  //------------------------------------------add new contact
   const contact = document.createElement("li");
   contact.classList.add("contact");
   contact.innerHTML = `
@@ -19,27 +20,50 @@ form.addEventListener("submit", (e) => {
     <button class="delete-contact">Delete</button>
   `;
   phoneBook.append(contact);
-//   ------------------------------------edit 
-  contact.querySelector(".edit-contact").addEventListener("click", (editEvent) => {
-    const phoneText = contact.querySelector(".phone");
-    const nameText = contact.querySelector(".name");
-    if (editEvent.target.textContent === "Edit") {
-      phoneText.innerHTML = `<input class="edit-input" value="${phoneText.textContent.replace("Phone: ", "")}" />`;
-      nameText.innerHTML = `<input class="edit-input" value="${nameText.textContent.replace("Name: ", "")}" />`;
-      editEvent.target.textContent = "Save";
-    } else {
-      const newPhone = phoneText.querySelector("input").value;
-      const newName = nameText.querySelector("input").value;
-      phoneText.innerHTML = `Phone: ${newPhone}`;
-      nameText.innerHTML = `Name: ${newName}`;
-      editEvent.target.textContent = "Edit";
-    }
-  });
+  //   ------------------------------------edit
+  contact
+    .querySelector(".edit-contact")
+    .addEventListener("click", (editEvent) => {
+      const phoneText = contact.querySelector(".phone");
+      const nameText = contact.querySelector(".name");
+      if (editEvent.target.textContent === "Edit") {
+        phoneText.innerHTML = `<input class="edit-input" value="${phoneText.textContent.replace(
+          "Phone: ",
+          ""
+        )}" />`;
+        nameText.innerHTML = `<input class="edit-input" value="${nameText.textContent.replace(
+          "Name: ",
+          ""
+        )}" />`;
+        editEvent.target.textContent = "Save";
+      } else {
+        const newPhone = phoneText.querySelector("input").value;
+        const newName = nameText.querySelector("input").value;
+        phoneText.innerHTML = `Phone: ${newPhone}`;
+        nameText.innerHTML = `Name: ${newName}`;
+        editEvent.target.textContent = "Edit";
+      }
+    });
 
   contact.querySelector(".delete-contact").addEventListener("click", () => {
     contact.remove();
   });
 
+  // search
+  search.addEventListener("input", (e) => {
+    const searcInput = e.target.value.toLowerCase();
+    const searchContacts = document.querySelectorAll("#phonebook .contact");
+
+    searchContacts.forEach((contact) => {
+      const name = contact.querySelector(".name").textContent.toLowerCase();
+      const phone = contact.querySelector(".phone").textContent.toLowerCase();
+      if (name.includes(searcInput) || phone.includes(searcInput)) {
+        contact.style.display = "flex";
+      } else {
+        contact.style.display = "none";
+      }
+    });
+  });
 
   form.reset();
 });
